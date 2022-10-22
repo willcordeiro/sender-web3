2e7f4749aca8327b2cc553c0a9fe5d393f10e2bb
willcordeiro <willcordeiro800@gmail.com>
2022-10-22 16:07:46 -0300

    added shorten adress

--- web3-sender/src/components/Welcome.jsx
+++ web3-sender/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { TransactionContext } from "../context/TransactionContext";
-// import { shortenAddress } from "../utils/shortenAddress";
+import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const companyCommonStyles =
@@ -39,8 +39,6 @@ const Welcome = () => {
 
     sendTransaction();
   };
-
-  function shortenAddress(t) {}
 
   return (
     <div className="flex w-full justify-center items-center">
