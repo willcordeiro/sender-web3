4d23e16e4ce0f4a07d4dba7cfe81932fbe3c2119
willcordeiro <willcordeiro800@gmail.com>
2022-10-22 14:46:19 -0300

    added the hardhat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY = "";
+
+const GOERLI_PRIVATE_KEY = "";
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
